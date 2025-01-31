f94cfbe85cdc7e7ef94f24baa61950fe1137ccf1
gnc <52561965+gncdev@users.noreply.github.com>
2025-02-01 00:23:49 +0300

    chrome_uninstaller.sh

---
+++ chrome_uninstaller.sh
@@ -0,0 +1,19 @@
+# about chrome
+rm -rf /Applications/Google\ Chrome.app/
+rm -rf ~/Applications/Chrome\ Apps.localized/
+rm -rf ~/Library/Application\ Support/Google/Chrome/
+rm -rf ~/Library/Application\ Support/CrashReporter/Google\ Chrome*
+rm -rf ~/Library/Caches/com.google.Chrome*
+rm -rf ~/Library/Saved\ Application\ State/com.google.Chrome.savedState/
+rm -rf ~/Library/Preferences/com.google.Chrome*
+rm -rf ~/Library/Preferences/Google\ Chrome*
+rm -rf ~/Library/Google/GoogleSoftwareUpdate/Actives/com.google.Chrome
+rm -rf ~/Library/Google/Google\ Chrome*
+
+# about keystone
+rm ~/Library/LaunchAgents/com.google.keystone.agent.plist
+rm ~/Library/Preferences/com.google.Keystone.Agent.plist
+rm -rf ~/Library/Caches/com.google.Keystone*
+rm -rf ~/Library/Caches/com.google.SoftwareUpdate
+rm -rf ~/Library/Application\ Support/Google/RLZ
+rm ~/Library/LaunchAgents/com.google.keystone*
